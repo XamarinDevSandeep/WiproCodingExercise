3b3e9e08bc700fcf6ffab1f2ffb18aab38a45c3f
SandeepD <sandeepdabhade23@gmail.com>
2018-02-15 21:02:01 +0530

    Project structure

---
+++ CodingExercise/CodingExercise/CodingExercise/View/FactsPage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace CodingExercise.View
+{
+    public class FactsPage : ContentPage
+    {
+        public FactsPage()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Welcome to Xamarin.Forms!" }
+                }
+            };
+        }
+    }
+}
